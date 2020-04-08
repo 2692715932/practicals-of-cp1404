1c968dab91fbeff588f4094ddb9c75eb0051c514
2692715932 <changlin.wen@my.jcu.edu.au>
2020-04-08 19:56:35 +0800

    Add password check program

--- prac03/password_check.py
+++ prac03/password_check.py
@@ -1,6 +1,12 @@
 def main():
-    password_of_user = (input("Enter your password:"))
+    password_of_user = get_password()
     length_of_password = len(password_of_user)
     print("*"*length_of_password)
+
+
+def get_password():
+    password_of_user = (input("Enter your password:"))
+    return password_of_user
+
 
 main()
